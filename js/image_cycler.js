062b51772bb7d31d5b961d8a98635fb208ac9f8d
Christophe Bedard <bedard.christophe@gmail.com>
2019-07-13 11:13:48 +0200

    Shuffle background images

--- js/image_cycler.js
+++ js/image_cycler.js
@@ -9,6 +9,17 @@ const imgs = [
     {% endfor %}
 ];
 
+// From: https://stackoverflow.com/a/6274381
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
+
+shuffle(imgs);
+
 // From: https://stackoverflow.com/a/37243062
 function setImg(image, container) {
     container.style.backgroundImage = `url(${image})`;
